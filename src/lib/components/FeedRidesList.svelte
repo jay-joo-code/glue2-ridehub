8cba93d8076846677d72853d2a7091c15e5606a7
Jay Joo <jj534@cornell.edu>
2023-02-18 13:53:43 -0500

    only fetch rides on client side

--- src/lib/components/FeedRidesList.svelte
+++ src/lib/components/FeedRidesList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import { pb } from '$lib/glue/pocketbase';
 	import { sub } from 'date-fns';
@@ -7,46 +8,49 @@
 	let rides: any[] = [];
 
 	$: {
-		(async () => {
-			// filter: location
-			const origin = $page.url?.searchParams?.get('origin');
-			const originFilter = origin ? `origin~"${origin?.toLowerCase()}"` : '';
-			const destination = $page.url?.searchParams?.get('destination');
-			const destinationFilter = destination ? `destination~"${destination?.toLowerCase()}"` : '';
+		if (browser) {
+			(async () => {
+				// filter: location
+				const origin = $page.url?.searchParams?.get('origin');
+				const originFilter = origin ? `origin~"${origin?.toLowerCase()}"` : '';
+				const destination = $page.url?.searchParams?.get('destination');
+				const destinationFilter = destination ? `destination~"${destination?.toLowerCase()}"` : '';
 
-			// filter: date (departure date)
-			// subtract 2 days, to get rides in nearby dates
-			const date = $page.url?.searchParams?.get('date');
-			const dateFilter = date ? `date>="${sub(new Date(date), { days: 2 })?.toISOString()}"` : '';
+				// filter: date (departure date)
+				// subtract 2 days, to get rides in nearby dates
+				const date = $page.url?.searchParams?.get('date');
+				const dateFilter = date ? `date>="${sub(new Date(date), { days: 2 })?.toISOString()}"` : '';
 
-			// filter: variant
-			const isShowDrivers = $page.url?.searchParams?.get('is-show-drivers') === 'true';
-			const isShowRiders = $page.url?.searchParams?.get('is-show-riders') === 'true';
-			let variantFilter = '';
-			if (isShowDrivers && !isShowRiders) {
-				variantFilter = 'variant="driver"';
-			} else if (!isShowDrivers && isShowRiders) {
-				variantFilter = 'variant="rider"';
-			}
+				// filter: variant
+				const isShowDrivers = $page.url?.searchParams?.get('is-show-drivers') === 'true';
+				const isShowRiders = $page.url?.searchParams?.get('is-show-riders') === 'true';
+				let variantFilter = '';
+				if (isShowDrivers && !isShowRiders) {
+					variantFilter = 'variant="driver"';
+				} else if (!isShowDrivers && isShowRiders) {
+					variantFilter = 'variant="rider"';
+				}
 
-			// filter: available only
-			const isShowAvailableOnly = $page.url?.searchParams?.get('is-show-available-only') === 'true';
-			const showAvailableOnlyFilter = isShowAvailableOnly ? 'isUnavailable=false' : '';
+				// filter: available only
+				const isShowAvailableOnly =
+					$page.url?.searchParams?.get('is-show-available-only') === 'true';
+				const showAvailableOnlyFilter = isShowAvailableOnly ? 'isUnavailable=false' : '';
 
-			const filters = [
-				originFilter,
-				destinationFilter,
-				dateFilter,
-				variantFilter,
-				showAvailableOnlyFilter
-			];
+				const filters = [
+					originFilter,
+					destinationFilter,
+					dateFilter,
+					variantFilter,
+					showAvailableOnlyFilter
+				];
 
-			rides = await pb.collection('rides').getFullList(200, {
-				filter: filters?.filter((filter) => filter)?.join('&&'),
-				expand: 'user',
-				sort: '-updated'
-			});
-		})();
+				rides = await pb.collection('rides').getFullList(200, {
+					filter: filters?.filter((filter) => filter)?.join('&&'),
+					expand: 'user',
+					sort: '-updated'
+				});
+			})();
+		}
 	}
 </script>
 
