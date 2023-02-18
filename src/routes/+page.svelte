f8e28b36f4ac6771f01fb4fd165be2b95b46ba30
Jay Joo <jj534@cornell.edu>
2023-02-18 10:41:16 -0500

    collapse filters to a modal on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,29 @@
 	import PageContainer from '$lib/components/glue/PageContainer.svelte';
 	import FeedRidesList from '$lib/components/FeedRidesList.svelte';
 	import RideFilters from '$lib/components/RideFilters.svelte';
+	import IconFilterList from '$lib/icons/glue/IconFilterList.svelte';
 </script>
 
 <PageContainer title="Home" layout="aside-main">
 	<Aside>
-		<RideFilters />
+		<div class="hidden md:block">
+			<RideFilters />
+		</div>
+		<div class="block md:hidden">
+			<!-- modal trigger -->
+			<label for="modal-ride-filters" class="btn-primary btn-sm btn gap-2"
+				><span class="text-xl"><IconFilterList /></span> Filters</label
+			>
+
+			<!-- modal content -->
+			<input type="checkbox" id="modal-ride-filters" class="modal-toggle" />
+			<label for="modal-ride-filters" class="modal cursor-pointer">
+				<label class="modal-box relative" for="">
+					<RideFilters />
+					<label for="modal-ride-filters" class="btn-primary btn-block btn mt-4">Done</label>
+				</label>
+			</label>
+		</div>
 	</Aside>
 	<Main>
 		<FeedRidesList />
