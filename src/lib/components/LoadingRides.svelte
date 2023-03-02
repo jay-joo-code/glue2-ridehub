ddef51007587120eccc5dee1d493e693c505a3c1
Jacob L <jrl359@cornell.edu>
2023-03-02 18:56:38 -0500

    Refinded loading states

---
+++ src/lib/components/LoadingRides.svelte
@@ -0,0 +1,104 @@
+<div class="relative">
+	<div class="relative">
+		<div
+			class="absolute inset-0 z-10 flex items-center justify-center bg-base-200/20 drop-shadow-2xl"
+		>
+			<button class="loading btn-primary btn">loading rides</button>
+		</div>
+		<div
+			class="h-64 space-y-4 rounded-lg border border-b border-base-300 border-blue-300 p-4 px-2 py-6 shadow"
+		>
+			<div class="flex animate-pulse space-x-4">
+				<div class="h-10 w-10 rounded-full bg-slate-200" />
+				<div class="flex-1 space-y-6 py-1">
+					<div class="h-2 rounded bg-slate-200" />
+					<div class="space-y-3">
+						<div class="grid grid-cols-3 gap-4">
+							<div class="col-span-2 h-2 rounded bg-slate-200" />
+							<div class="col-span-1 h-2 rounded bg-slate-200" />
+						</div>
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="grid grid-cols-3 gap-4">
+							<div class="col-span-2 h-2 rounded bg-slate-200" />
+							<div class="col-span-1 h-2 rounded bg-slate-200" />
+						</div>
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="relative">
+		<div
+			class="absolute inset-0 z-10 flex items-center justify-center bg-base-200/20 drop-shadow-2xl"
+		>
+			<button class="loading btn-primary btn">loading rides</button>
+		</div>
+		<div
+			class="my-2 h-64 space-y-4 rounded-lg border border-b border-base-300 border-blue-300 p-4 px-2 py-6 shadow"
+		>
+			<div class="flex animate-pulse space-x-4">
+				<div class="h-10 w-10 rounded-full bg-slate-200" />
+				<div class="flex-1 space-y-6 py-1">
+					<div class="h-2 rounded bg-slate-200" />
+					<div class="space-y-3">
+						<div class="grid grid-cols-3 gap-4">
+							<div class="col-span-2 h-2 rounded bg-slate-200" />
+							<div class="col-span-1 h-2 rounded bg-slate-200" />
+						</div>
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="grid grid-cols-3 gap-4">
+							<div class="col-span-2 h-2 rounded bg-slate-200" />
+							<div class="col-span-1 h-2 rounded bg-slate-200" />
+						</div>
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="relative">
+		<div
+			class="absolute inset-0 z-10 flex items-center justify-center bg-base-200/20 drop-shadow-2xl"
+		>
+			<button class="loading btn-primary btn">loading rides</button>
+		</div>
+		<div
+			class="my-2 h-64 space-y-4 rounded-lg border border-b border-base-300 border-blue-300 p-4 px-2 py-6 shadow"
+		>
+			<div class="flex animate-pulse space-x-4">
+				<div class="h-10 w-10 rounded-full bg-slate-200" />
+				<div class="flex-1 space-y-6 py-1">
+					<div class="h-2 rounded bg-slate-200" />
+					<div class="space-y-3">
+						<div class="grid grid-cols-3 gap-4">
+							<div class="col-span-2 h-2 rounded bg-slate-200" />
+							<div class="col-span-1 h-2 rounded bg-slate-200" />
+						</div>
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="grid grid-cols-3 gap-4">
+							<div class="col-span-2 h-2 rounded bg-slate-200" />
+							<div class="col-span-1 h-2 rounded bg-slate-200" />
+						</div>
+						<div class="h-2 rounded bg-slate-200" />
+						<div class="h-2 rounded bg-slate-200" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
